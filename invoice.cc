757aa155327b6a92123b4e9a034753f9c34acfac
Chloe Hendrikse <ch881518@ohio.edu>
2020-09-14 09:58:04 -0500

    Final

--- invoice.cc
+++ invoice.cc
@@ -1,9 +1,9 @@
 /*
  *        File: invoice.cc
- *      Author: Enter your name
- *        Date: Enter the date
+ *      Author: Chloe Hendrikse
+ *        Date: 9/13/2020
  * Description: 
- *              Add Description
+ *              Invoice HW for CS2400
  */
 
 #include <iostream>
@@ -13,6 +13,75 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 
-    /*add code*/
+    string residency, driveType, response, brand; // variable declaration
+    double number, total, subtotal, quantity, price, discount, taxes, tprice;
+    cout << "Enter a hard drive type (w, s) followed by quantity: ";
+    cin >> driveType;
+    cin >> quantity; //inputting drive type and quantity of drives
+    if (driveType == "w")
+    {
+        price = 90.00;
+        brand = "Western Digital";
+    }
+    if (driveType == "s")
+    {
+        brand = "Seagate";
+        price = 95.00;
+    }
+    if (quantity >= 5)
+    {
+        tprice = quantity * price;
+        number = quantity;
+        discount = .05 * price;
+        subtotal = quantity * (price - discount);
+    }
+    else
+    {
+        tprice = quantity * price;
+        number = quantity;
+        discount = 0;
+        subtotal = quantity * price;
+    }
+    
+
+
+       cout << "Do you live in Ohio? (yes or no)";
+   cin >> residency; //where they input residency
+   if (residency == "yes")
+   {
+       taxes = (.7 * subtotal);
+       total = subtotal + taxes;
+   }
+   if (residency == "no")
+   {
+       taxes = 0;
+       total = subtotal + taxes;
+   }
+   
+
+   // Invoice
+   cout << "Hard Drives Invoice" << endl;
+   cout << "---------------------------" << endl;
+   cout << "Drive Brand: " << brand << endl;
+   cout << "Quantity: " << number << endl;
+   cout << "Price: $" << price << endl;
+   cout << "Ohio resident: " + residency << endl;
+   cout << "Total price: $" << tprice << endl;
+   cout << "---------------------------" << endl;
+   cout << "Discount: $" << discount << endl;
+   cout << "Subtotal: $";
+   cout << subtotal << endl;
+   cout << "Taxes: $";
+   cout << taxes << endl;
+   cout << "===========================" << endl;
+   cout << "Total; $"; 
+   cout << total << endl;
+   cout << "===========================";
+
+
+
+
+   
+
     return 0;
 }// main
